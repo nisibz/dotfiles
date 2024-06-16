18570e37a121b7cb9d6b47d340f5d1a9b2896cfb
Nantachai Sangadsri <bz.nantachai@gmail.com>
2024-06-16 12:59:03 +0000

    Add gitconfig file and install oh-my-zsh, brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,23 @@ sudo apt update && sudo apt upgrade -y
 echo "installing zsh"
 sudo apt install zsh
 
+# Install oh-my-zsh
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install brew
+echo "Installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install Homebrew's dependencies
+sudo apt-get install build-essential -y
+
 # Symlink dotfiles to home directory
 echo "Symlinking dotfiles..."
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # Change default shell to zsh
 chsh -s "$(command -v zsh)"
