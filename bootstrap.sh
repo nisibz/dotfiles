9a5b4fce88f5b206a3d6f408f9b8675cc04f53ea
Nisibz <48269425+nisibz@users.noreply.github.com>
2024-06-18 03:28:43 +0000

    add fucntion for check zsh plugin is already installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,24 +21,46 @@ else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Function to check if a plugin is already installed
+zsh_plugin_exists() {
+    local plugin_path="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$1"
+    [ -d "$plugin_path" ]
+}
+
 # Install Oh My Zsh plugins
 echo "Installing Oh My Zsh plugins..."
 
-# Install zsh-autosuggestions
-echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Install zsh-autosuggestions if not already installed
+if zsh_plugin_exists "zsh-autosuggestions"; then
+    echo "zsh-autosuggestions is already installed"
+else
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-# Install zsh-syntax-highlighting
-echo "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Install zsh-syntax-highlighting if not already installed
+if zsh_plugin_exists "zsh-syntax-highlighting"; then
+    echo "zsh-syntax-highlighting is already installed"
+else
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
-# Install you-should-use
-echo "Installing you-should-use..."
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+# Install you-should-use if not already installed
+if zsh_plugin_exists "you-should-use"; then
+    echo "you-should-use is already installed"
+else
+    echo "Installing you-should-use..."
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use
+fi
 
-# Install zsh-bat
-echo "Installing zsh-bat..."
-git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
+# Install zsh-bat if not already installed
+if zsh_plugin_exists "zsh-bat"; then
+    echo "zsh-bat is already installed"
+else
+    echo "Installing zsh-bat..."
+    git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-bat
+fi
 
 
 # Symlink dotfiles to home directory
