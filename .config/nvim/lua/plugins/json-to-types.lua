08f5fd295be526734a2064d41174ede66cc9f09b
Nantachai <bz.nantachai@gmail.com>
2024-10-21 01:58:43 +0700

    Add json-to-types and vim-tmux-navigator plugins configurations.

---
+++ .config/nvim/lua/plugins/json-to-types.lua
@@ -0,0 +1,16 @@
+return {
+  "Redoxahmii/json-to-types.nvim",
+  build = "sh install.sh npm",
+  -- keys = {
+  --   {
+  --     "<leader>cu",
+  --     "<CMD>ConvertJSONtoLang typescript<CR>",
+  --     desc = "Convert JSON to TS",
+  --   },
+  --   {
+  --     "<leader>ct",
+  --     "<CMD>ConvertJSONtoLangBuffer typescript<CR>",
+  --     desc = "Convert JSON to TS in buffer",
+  --   },
+  -- },
+}
