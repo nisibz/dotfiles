91bd70edc89de50d02070dfd315e9bc5f7772e76
Nantachai Sangadsri <bz.nantachai@gmail.com>
2025-03-24 14:57:47 +0700

    add hold esc to ctrl in base and work layout and change del to bspc in nav layout

--- silakka54/keymaps/main_config/keymap.c
+++ silakka54/keymaps/main_config/keymap.c
@@ -13,20 +13,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                             KC_LGUI, MO(_NAV), KC_SPC,           KC_SPC,  MO(_SYM), KC_RALT
     ),
     [_WORK] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,    KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
+        MT(MOD_LCTL, KC_ESC),  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,    KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                             KC_LGUI, MO(_NAV), KC_SPC,           KC_SPC,  MO(_SYM), KC_RALT
     ),
     [_NAV] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_RBRC,
         _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
                                             _______, _______, _______,          _______, MO(_FN), _______
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │  TAB  │   Q   │   W   │   E   │   R   │   T   │                 │   Y   │   U   │   I   │   O   │   P   │ BSPC  │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│  ESC  │ A/GUI │ S/ALT │D/SHIFT│ F/CTL │   G   │                 │   H   │ J/CTL │K/SHIFT│ L/ALT │ ;/GUI │   '   │
+│ESC/CTL│   A   │   S   │   D   │   F   │   G   │                 │   H   │   J   │   K   │   L   │   ;   │   '   │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │ SHIFT │   Z   │   X   │   C   │   V   │   B   │                 │   N   │   M   │   ,   │   .   │   /   │ ENTER │
 └───────┴───────┴───────┴───────┼───────┼───────┼───────┐ ┌───────┼───────┼───────┼───────┴───────┴───────┴───────┘
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │  TAB  │   Q   │   W   │   E   │   R   │   T   │                 │   Y   │   U   │   I   │   O   │   P   │ BSPC  │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│  ESC  │   A   │   S   │   D   │   F   │   G   │                 │   H   │   J   │   K   │   L   │   ;   │   '   │
+│ESC/CTL│ A/GUI │ S/ALT │D/SHIFT│ F/CTL │   G   │                 │   H   │ J/CTL │K/SHIFT│ L/ALT │ ;/GUI │   '   │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │ SHIFT │   Z   │   X   │   C   │   V   │   B   │                 │   N   │   M   │   ,   │   .   │   /   │ ENTER │
 └───────┴───────┴───────┴───────┼───────┼───────┼───────┐ ┌───────┼───────┼───────┼───────┴───────┴───────┴───────┘
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ┌───────┬───────┬───────┬───────┬───────┬───────┐                 ┌───────┬───────┬───────┬───────┬───────┬───────┐
 │       │       │       │       │       │       │                 │       │       │       │       │       │       │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│   `   │   1   │   2   │   3   │   4   │   5   │                 │   6   │   7   │   8   │   9   │   0   │  DEL  │
+│   `   │   1   │   2   │   3   │   4   │   5   │                 │   6   │   7   │   8   │   9   │   0   │ BSPC  │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │       │  F1   │  F2   │  F3   │  F4   │  F5   │                 │  LEFT │ DOWN  │  UP   │ RIGHT │       │   ]   │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
