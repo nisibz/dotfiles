5bac3d2504f89c4a97d2e7b6200a366b82527da1
Nantachai Sangadsri <bz.nantachai@gmail.com>
2025-04-11 12:15:24 +0700

    refactor: rename WORK layer to GAME and adjust keymaps

--- silakka54/keymaps/main_config/keymap.c
+++ silakka54/keymaps/main_config/keymap.c
@@ -2,7 +2,7 @@
 
 enum layer_names {
     _BASE,
-    _WORK,
+    _GAME,
     _NAV,
     _SYM,
     _FN
@@ -13,14 +13,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        MT(MOD_LCTL, KC_ESC),  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,    KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                             KC_LGUI, MO(_NAV), KC_SPC,           KC_SPC,  MO(_SYM), KC_RALT
     ),
-    [_WORK] = LAYOUT(
+    [_GAME] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        MT(MOD_LCTL, KC_ESC),  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G,    KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
+        MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                             KC_LGUI, MO(_NAV), KC_SPC,           KC_SPC,  MO(_SYM), KC_RALT
     ),
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
         QK_BOOT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV,                            KC_MNXT, KC_BRID, KC_BRIU, BL_DOWN, BL_UP,   BL_TOGG,
         RGB_TOG, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI,                            RGB_VAD, RGB_MOD, KC_MSEL, KC_EJCT, KC_CALC, KC_PSCR,
-        _______, TG(_WORK), _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+        _______, TG(_GAME), _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
                                             _______, _______, _______,          _______, _______, _______
     ),
 };
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │  TAB  │   Q   │   W   │   E   │   R   │   T   │                 │   Y   │   U   │   I   │   O   │   P   │ BSPC  │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│ESC/CTL│   A   │   S   │   D   │   F   │   G   │                 │   H   │   J   │   K   │   L   │   ;   │   '   │
+│ESC/CTL│ A/GUI │ S/ALT │D/SHIFT│ F/CTL │   G   │                 │   H   │ J/CTL │K/SHIFT│ L/ALT │ ;/GUI │   '   │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │ SHIFT │   Z   │   X   │   C   │   V   │   B   │                 │   N   │   M   │   ,   │   .   │   /   │ ENTER │
 └───────┴───────┴───────┴───────┼───────┼───────┼───────┐ ┌───────┼───────┼───────┼───────┴───────┴───────┴───────┘
@@ -63,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 /*
-[_WORK] = LAYOUT(
+[_GAME] = LAYOUT(
 ┌───────┬───────┬───────┬───────┬───────┬───────┐                 ┌───────┬───────┬───────┬───────┬───────┬───────┐
 │       │       │       │       │       │       │                 │       │       │       │       │       │       │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │  TAB  │   Q   │   W   │   E   │   R   │   T   │                 │   Y   │   U   │   I   │   O   │   P   │ BSPC  │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│ESC/CTL│ A/GUI │ S/ALT │D/SHIFT│ F/CTL │   G   │                 │   H   │ J/CTL │K/SHIFT│ L/ALT │ ;/GUI │   '   │
+│ESC/CTL│   A   │   S   │   D   │   F   │   G   │                 │   H   │   J   │   K   │   L   │   ;   │   '   │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │ SHIFT │   Z   │   X   │   C   │   V   │   B   │                 │   N   │   M   │   ,   │   .   │   /   │ ENTER │
 └───────┴───────┴───────┴───────┼───────┼───────┼───────┐ ┌───────┼───────┼───────┼───────┴───────┴───────┴───────┘
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
 │RGBTOG │ HUE+  │ HUE-  │ SAT+  │ SAT-  │ VAL+  │                 │ VAL-  │ RMODE │ MSEL  │ EJECT │ CALC  │ PRINT │
 ├───────┼───────┼───────┼───────┼───────┼───────┤                 ├───────┼───────┼───────┼───────┼───────┼───────┤
-│       │ WORK  │       │       │       │       │                 │       │       │       │       │       │       │
+│       │ GAME  │       │       │       │       │                 │       │       │       │       │       │       │
 └───────┴───────┴───────┴───────┼───────┼───────┼───────┐ ┌───────┼───────┼───────┼───────┴───────┴───────┴───────┘
                                 │       │       │       │ │       │       │       │
                                 └───────┴───────┴───────┘ └───────┴───────┴───────┘
