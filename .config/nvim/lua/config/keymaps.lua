21aa28d3a0a98ca90ba924d40bae3ad0ebd9b917
Nantachai Sangadsri <bz.nantachai@gmail.com>
2025-06-12 15:24:39 +0700

    refactor: replace manual copy keymaps with copy-file-path plugin keybindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>ya", function()
-  vim.fn.setreg("+", vim.fn.expand("%:p"))
-end, { desc = "Copy absolute directory of current file to clipboard" })
-
-vim.keymap.set("n", "<leader>yc", function()
-  vim.fn.setreg("+", vim.fn.expand("%"))
-end, { desc = "Copy current working directory to clipboard" })
-
 vim.keymap.set("n", "<leader>xr", "<cmd>Rest run<cr>", { desc = "Run REST request under cursor" })
--- .config/nvim/lua/plugins/copy-file-path.lua
+++ .config/nvim/lua/plugins/copy-file-path.lua
@@ -1,6 +1,9 @@
 return {
   "h3pei/copy-file-path.nvim",
-  config = function()
-    -- Same keymaps as above
-  end,
+  keys = {
+    { "<leader>yp", "<cmd>CopyRelativeFilePath<cr>", desc = "Copy relative file path" },
+    { "<leader>ya", "<cmd>CopyAbsoluteFilePath<cr>", desc = "Copy absolute file path" },
+    { "<leader>yh", "<cmd>CopyRelativeFilePathFromHome<cr>", desc = "Copy relative path from home" },
+    { "<leader>yn", "<cmd>CopyFileName<cr>", desc = "Copy file name" },
+  },
 }
