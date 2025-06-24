7a8580301924ea42ae53e3dd1cc45030cb650b85
Nantachai Sangadsri <bz.nantachai@gmail.com>
2025-06-24 16:23:35 +0700

    chore: remove LazyVim Neovim config and disable version manager installs

--- chezmoi/dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<leader>xr", "<cmd>Rest run<cr>", { desc = "Run REST request under cursor" })
--- chezmoi/dot_config/nvim/lua/plugins/copy-file-path.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "h3pei/copy-file-path.nvim",
-  keys = {
-    { "<leader>yp", "<cmd>CopyRelativeFilePath<cr>", desc = "Copy relative file path" },
-    { "<leader>ya", "<cmd>CopyAbsoluteFilePath<cr>", desc = "Copy absolute file path" },
-    { "<leader>yh", "<cmd>CopyRelativeFilePathFromHome<cr>", desc = "Copy relative path from home" },
-    { "<leader>yn", "<cmd>CopyFileName<cr>", desc = "Copy file name" },
-  },
-}
