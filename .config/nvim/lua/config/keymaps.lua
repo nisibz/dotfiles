e0cd09f58e37d2e27a7d89e9ba6372dc149cfb4c
Nantachai Sangadsri <bz.nantachai@gmail.com>
2026-01-28 17:08:54 +0700

    feat: add lazydocker keymap and refactor to LazyVim.safe_keymap_set

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>xr", "<cmd>Rest run<cr>", { desc = "Run REST request under cursor" })
+-- This file is automatically loaded by lazyvim.config.init
+
+-- DO NOT USE `LazyVim.safe_keymap_set` IN YOUR OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = LazyVim.safe_keymap_set
+
+-- rest.nvim
+map("n", "<leader>xr", "<cmd>Rest run<cr>", { desc = "Run REST request under cursor" })
+
+-- lazydocker
+if vim.fn.executable("lazydocker") == 1 then
+  map("n", "<leader>gd", function()
+    Snacks.terminal({ "lazydocker" })
+  end, { desc = "LazyDocker" })
+end
