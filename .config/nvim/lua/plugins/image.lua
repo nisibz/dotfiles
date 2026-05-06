8b8be0c7060f5980c01a34f99b1ee90dafd99164
Nantachai Sangadsri <bz.nantachai@gmail.com>
2026-05-06 16:31:17 +0700

    feat: configure image.nvim with kitty backend and markdown integration

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -4,4 +4,19 @@ return {
   opts = {
     processor = "magick_cli",
   },
+  config = function()
+    require("image").setup({
+      backend = "kitty",
+      kitty_method = "normal",
+      integrations = {
+        markdown = {
+          enabled = true,
+        },
+      },
+      max_width_window_percentage = 100,
+      max_height_window_percentage = 100,
+      window_overlap_clear_enabled = true,
+      scale_factor = 3.0,
+    })
+  end,
 }
